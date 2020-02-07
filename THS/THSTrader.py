ae0fa9da684bfc9d850b5d6968491310529680a7
Tu_Home <956168327@qq.com>
2020-02-07 21:19:01 +0800

    auto trade

--- THS/THSTrader.py
+++ THS/THSTrader.py
@@ -10,14 +10,12 @@ import time
 
 class THSTrader:
 
-    def __init__(self, exe_path=r"C:\同花顺软件\同花顺\xiadan.exe"): 
+    def __init__(self, exe_path=r"C:\同花顺软件\同花顺\xiadan.exe"):
         print("正在连接客户端:", exe_path, "......")
         self.app = pywinauto.Application().connect(path=exe_path, timeout=10)
         print("连接成功!!!")
         self.main_wnd = self.app.top_window()
-    
-    
-    
+
     def buy(self, stock_no, price, amount):
         """ 买入 """
         time.sleep(1)
@@ -34,7 +32,8 @@ class THSTrader:
         """ 撤单 """
         time.sleep(1)
         self.__select_menu(['撤单[F3]'])
-        cancelable_entrusts = self.__get_grid_data(is_entrust=True)  # 获取可以撤单的条目
+        cancelable_entrusts = self.__get_grid_data(
+            is_entrust=True)  # 获取可以撤单的条目
 
         for i, entrust in enumerate(cancelable_entrusts):
             if str(entrust["合同编号"]) == str(entrust_no):  # 对指定合同进行撤单
@@ -49,10 +48,11 @@ class THSTrader:
         result = {}
         for key, control_id in BALANCE_CONTROL_ID_GROUP.items():
             result[key] = float(
-                self.main_wnd.window(control_id=control_id, class_name='Static').window_text()
+                self.main_wnd.window(control_id=control_id,
+                                     class_name='Static').window_text()
             )
         return result
-    
+
     def check_trade_finished(self, entrust_no):
         """ 判断订单是否完成 """
         time.sleep(1)
@@ -66,7 +66,6 @@ class THSTrader:
                 if entrust["成交数量"] == 0:
                     return False
         return True
-    
 
     def get_position(self):
         """ 获取持仓 """
@@ -86,38 +85,48 @@ class THSTrader:
         return self.__get_grid_data()
 
     def __trade(self, stock_no, price, amount):
+        print(stock_no, price, amount)
         time.sleep(0.2)
-        self.main_wnd.window(control_id=0x408, class_name="Edit").set_text(str(stock_no))  # 设置股票代码
-        self.main_wnd.window(control_id=0x409, class_name="Edit").set_text(str(price))  # 设置价格
-        self.main_wnd.window(control_id=0x40A, class_name="Edit").set_text(str(amount))  # 设置股数目
+        self.main_wnd.window(control_id=0x408, class_name="Edit").type_keys(
+            str(stock_no))  # 设置股票代码
+        time.sleep(0.5)
+        # self.main_wnd.window(control_id=0x409, class_name="Edit").type_keys(
+        #     str(price))  # 设置价格
+        # time.sleep(0.5)
+        self.main_wnd.window(control_id=0x40A, class_name="Edit").type_keys(
+            str(int(amount)))  # 设置股数目
         time.sleep(0.2)
-        self.main_wnd.window(control_id=0x3EE, class_name="Button").click()   # 点击卖出or买入
-        
+        self.main_wnd.window(
+            control_id=0x3EE, class_name="Button").click()   # 点击卖出or买入
+
         time.sleep(0.2)
-        self.app.top_window().window(control_id=0x6, class_name='Button').click()  # 确定买入
-        self.app.top_window().set_focus()
-        time.sleep(0.2)
-        result = self.app.top_window().window(control_id=0x3EC, class_name='Static').window_text()
-        self.app.top_window().set_focus()
-        time.sleep(0.2)
-        try:
-            self.app.top_window().window(control_id=0x2, class_name='Button').click()  # 确定
-        except:
-            pass
-        
-        
-        return self.__parse_result(result)
+        # self.app.top_window().window(control_id=0x6, class_name='Button').click()  # 确定买入
+        # self.app.top_window().set_focus()
+        # time.sleep(0.2)
+        # result = self.app.top_window().window(
+        #     control_id=0x3EC, class_name='Static').window_text()
+        # self.app.top_window().set_focus()
+        # time.sleep(0.2)
+        # try:
+        #     self.app.top_window().window(control_id=0x2, class_name='Button').click()  # 确定
+        # except:
+        #     pass
+
+        # return self.__parse_result(result)
 
     def __get_grid_data(self, is_entrust=False):
         """ 获取grid里面的数据 """
         time.sleep(0.1)
-        grid = self.main_wnd.window(control_id=0x417, class_name='CVirtualGridCtrl')
+        grid = self.main_wnd.window(
+            control_id=0x417, class_name='CVirtualGridCtrl')
         time.sleep(0.1)
         grid.set_focus().right_click()  # 模拟右键
-        
+
         if not is_entrust:
             keyboard.SendKeys('{DOWN}')
             time.sleep(0.1)
+        keyboard.SendKeys('{DOWN}')
+        time.sleep(0.1)
         keyboard.SendKeys('{DOWN}')
         time.sleep(0.1)
         keyboard.SendKeys('{DOWN}')
@@ -126,19 +135,21 @@ class THSTrader:
         time.sleep(1)
 
         file_path = "tmp.png"
-        
-        while True:
+
+        while 0:
             self.app.top_window().window(control_id=0x965, class_name='Static').\
                 CaptureAsImage().save(file_path)  # 保存验证码
-    
+
             captcha_num = captcha_recognize(file_path)  # 识别验证码
             print("captcha result-->", captcha_num)
-            self.app.top_window().window(control_id=0x964, class_name='Edit').set_text(captcha_num)  # 模拟输入验证码
-            
+            self.app.top_window().window(
+                control_id=0x964, class_name='Edit').set_text(captcha_num)  # 模拟输入验证码
+
             self.app.top_window().set_focus()
             keyboard.SendKeys("{ENTER}")   # 模拟发送enter，点击确定
             try:
-                print(self.app.top_window().window(control_id=0x966, class_name='Static').window_text())
+                print(self.app.top_window().window(
+                    control_id=0x966, class_name='Static').window_text())
             except:
                 break
             time.sleep(0.1)
@@ -151,14 +162,16 @@ class THSTrader:
         """ 点击左边菜单 """
         if r"网上股票" not in self.app.top_window().window_text():
             self.app.top_window().set_focus()
-            pywinauto.keyboard.SendKeys("{ENTER}")  
+            pywinauto.keyboard.SendKeys("{ENTER}")
         self.__get_left_menus_handle().get_item(path).click()
 
     def __get_left_menus_handle(self):
         while True:
             try:
-                handle = self.main_wnd.window(control_id=129, class_name='SysTreeView32')
-                handle.wait('ready', 2)  # sometime can't find handle ready, must retry
+                handle = self.main_wnd.window(
+                    control_id=129, class_name='SysTreeView32')
+                # sometime can't find handle ready, must retry
+                handle.wait('ready', 2)
                 return handle
             except Exception as ex:
                 print(ex)
@@ -168,11 +181,13 @@ class THSTrader:
         """ 通过双击撤单 """
         x = 50
         y = 30 + 16 * row
-        self.app.top_window().window(control_id=0x417, class_name='CVirtualGridCtrl').double_click(coords=(x, y))
+        self.app.top_window().window(
+            control_id=0x417, class_name='CVirtualGridCtrl').double_click(coords=(x, y))
         self.app.top_window().window(control_id=0x6, class_name='Button').click()  # 确定撤单
         time.sleep(0.1)
         if "网上股票交易系统5.0" not in self.app.top_window().window_text():
-            result = self.app.top_window().window(control_id=0x3EC, class_name='Static').window_text()
+            result = self.app.top_window().window(
+                control_id=0x3EC, class_name='Static').window_text()
             self.app.top_window().window(control_id=0x2, class_name='Button').click()  # 确定撤单
             return self.__parse_result(result)
         else:
@@ -183,12 +198,12 @@ class THSTrader:
     @staticmethod
     def __parse_result(result):
         """ 解析买入卖出的结果 """
-        
+
         # "您的买入委托已成功提交，合同编号：865912566。"
         # "您的卖出委托已成功提交，合同编号：865967836。"
         # "您的撤单委托已成功提交，合同编号：865967836。"
         # "系统正在清算中，请稍后重试！ "
-        
+
         if r"已成功提交，合同编号：" in result:
             return {
                 "success": True,
@@ -200,4 +215,3 @@ class THSTrader:
                 "success": False,
                 "msg": result
             }
-
--- test.py
+++ test.py
@@ -10,13 +10,12 @@ if __name__ == "__main__":
     print(trader.get_balance())                            # 获取当前可用资金
 
     print(trader.get_position())                           # 获取当前持有的股票
+    print(trader.get_balance())
+    # print(trader.sell(stock_no="000622", price=4.42, amount=100))  # 卖出股票
 
-    print(trader.sell(stock_no="162411", amount=100, price=0.51))  # 卖出股票
+    # result = trader.buy(stock_no="000622", price=0.49, amount=100) # 买入股票
+    # print(result)
 
-    result = trader.buy(stock_no="162411", amount=100, price=0.49) # 买入股票
-    print(result)
-
-    if result["success"] == True:						   # 如果买入下单成功，尝试撤单
-        print("撤单测试--->", end="")
-        print(trader.cancel_entrust(entrust_no=result["entrust_no"]))
-
+    # if result["success"] == True:						   # 如果买入下单成功，尝试撤单
+    #     print("撤单测试--->", end="")
+    #     print(trader.cancel_entrust(entrust_no=result["entrust_no"]))
